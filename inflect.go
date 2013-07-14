cbdd95264cfb359400e10bbb6a584b8bc3a964e7
Chuck Preslar <chuckpreslar@gmail.com>
2013-07-14 01:04:54 -0400

    Initial documentation.

--- inflect.go
+++ inflect.go
@@ -5,14 +5,15 @@ import (
   "regexp"
 )
 
+// Rule struct represents a linguistic pluralization/singularization rule.
 type Rule struct {
-  Regexp  *regexp.Regexp
-  Replace string
-  Append  bool
+  Regexp  *regexp.Regexp // Regular expression the string must match.
+  Replace string         // String to replace a match with.
+  Append  bool           // Append to the end of a matched string?
 }
 
+// List of pluralization rules in order by precedence.
 var plurals = []Rule{
-  // Rules should be ordered by precedence.
   Rule{regexp.MustCompile(`(auto|kangaroo|kilo|memo|photo|piano|pimento|pro|solo|soprano|studio|tattoo|video|zoo)$`), `s`, true},
   Rule{regexp.MustCompile(`(ex|ix)$`), `ices`, false},
   Rule{regexp.MustCompile(`(oo)`), `ee`, false},
@@ -23,6 +24,8 @@ var plurals = []Rule{
   Rule{regexp.MustCompile(`(on)$`), `a`, false},
 }
 
+// Plural returns the pluralized form of the word if a
+// matched rule is found, else the original string is returned.
 func Plural(str string) string {
   for _, rule := range plurals {
     if rule.Regexp.MatchString(str) {
