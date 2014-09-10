2734b686192b2fa84d6f32a30f4c68852e9ad677
Chuck Preslar <chuckpreslar@gmail.com>
2014-09-10 13:02:42 -0600

    Use ToLower to compare irreglar strings.

--- types/irregular.go
+++ types/irregular.go
@@ -1,6 +1,8 @@
 // Package types contains common types useful to the inflect package.
 package types
 
+import "strings"
+
 // IrregularType provides a structure for irregular words that do not follow standard rules.
 type IrregularType struct {
 	Singular string // The singular form of the irregular word.
@@ -12,8 +14,9 @@ type IrregularsType []*IrregularType
 
 // IsIrregular returns an IrregularType and bool if the IrregularsType slice contains the word.
 func (self IrregularsType) IsIrregular(str string) (*IrregularType, bool) {
+	str = strings.ToLower(str)
 	for _, irregular := range self {
-		if irregular.Singular == str || irregular.Plural == str {
+		if strings.ToLower(irregular.Singular) == str || strings.ToLower(irregular.Plural) == str {
 			return irregular, true
 		}
 	}
